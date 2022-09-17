94eaa52e3226e3fb859c099975fe4e4b54237f4b
Avi Bhawnani <avibhawnani.in@gmail.com>
2022-09-17 19:24:54 +0530

    Update Alerts.js
    Fixed alert issue

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -5,11 +5,11 @@ function Alerts(props) {
     let lower = word.toLowerCase();
     return lower.charAt(0).toUpperCase()+lower.slice(1);
   }
-  return (
-   
-      props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+ return (
+      <div style={{height:'50px'}}>
+      {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
       <strong>{Capitalize(props.alert.type)} </strong> : {props.alert.msg}
-      
+      </div>}
       </div>
     
   );
